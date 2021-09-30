6b2faf79f309b5a206a4b4dcf90f7b1515e64027
chad <thechanmoon@gmail.com>
2021-09-30 16:59:05 -0500

    state added

--- src/App.js
+++ src/App.js
@@ -7,12 +7,24 @@ import './App.css';
 
 // function App() {
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:"WEB",sub:"World Wide Web!"},
+      contents: [
+        {id:1, title:"HTML"}
+      ]
+    }
+  }
   render(){
     return (
       <div className="App">
         {/* Hello, React!! */}
-        <Subject title = "WEB" sub = "world wide web!"></Subject>
-        <Subject title = "React" sub = "For UI"></Subject>
+        <Subject 
+          title = {this.state.subject.title} 
+          sub = {this.state.subject.sub}>
+        </Subject>
+        {/* <Subject title = "React" sub = "For UI"></Subject> */}
         <Toc></Toc>
         <Contents title = "HTML" desc = "HTML is HyperText Makrup Language."></Contents>
       </div >
